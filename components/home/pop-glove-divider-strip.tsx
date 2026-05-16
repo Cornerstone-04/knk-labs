4df93679bae6499390316f6708e0664b919e1643
Cornerstone <fortunecornerstone@gmail.com>
2026-05-16 11:00:04 +0100

    chore: create new hero for pop

---
+++ components/home/pop-glove-divider-strip.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Marquee from "react-fast-marquee";
+
+const ITEMS = [
+  "P.O.P Glove",
+  "Royal Pop Dock",
+  "Driver Interface",
+  "Limited First Run",
+  "Precision Visibility",
+  "Performance Wearable",
+  "AP x Swatch Royal Pop",
+  "Cockpit Integration",
+];
+
+export const PopGloveDividerStrip = () => {
+  return (
+    <div className="overflow-hidden border-b border-border bg-surface py-5">
+      <Marquee autoFill speed={25} gradient={false} pauseOnHover={false}>
+        {ITEMS.map((item, i) => (
+          <span
+            key={`${item}-${i}`}
+            className="mr-16 shrink-0 font-mono text-xxs tracking-[0.25em] normal-case text-white/50"
+          >
+            <span className="mr-4 text-orange">✦</span>
+            {item}
+          </span>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
